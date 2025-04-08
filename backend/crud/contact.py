4941116bc74f9de358fc3f2e506d41c438664b46
rleigh1 <rleigh@turnitin.com>
2025-04-08 09:52:50 -0400

    updating contact crud operations

--- backend/crud/contact.py
+++ backend/crud/contact.py
@@ -1,8 +1,9 @@
+from sqlalchemy import delete
 from sqlalchemy.future import select
 from sqlalchemy.sql import Update
 from sqlalchemy.orm import joinedload, selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
-from schemas.schemas import ContactUpdate
+from schemas.schemas import ContactCreate, ContactUpdate
 from models import Contact
 
 
@@ -33,15 +34,14 @@ async def get_contact_with_details(db: AsyncSession, contact_id: int):
     result = await db.execute(stmt)  # Async execution
     return result.scalars().first()  # Extract first result
 
-# TODO: fix this function
-async def create_contact(contact: Contact, db: AsyncSession):
-  db.add(contact)  # Add the new contact to the session
-  try:
-    await db.commit()  # Commit the transaction to save the new contact
-  except Exception as e:
-    await db.rollback()  # Rollback in case of an error
-    raise e  # Re-raise the exception for handling upstream
-
+async def create_contact(request_body: ContactCreate, db: AsyncSession):
+  new_contact = Contact(**request_body.model_dump())
+  db.add(new_contact)
+  await db.commit()
+  await db.refresh(new_contact)
+  
+  return new_contact
+  
 async def update_contact(contact_id: int, request_body: ContactUpdate, db: AsyncSession):
     stmt = (
       Update(Contact)
@@ -58,3 +58,13 @@ async def update_contact(contact_id: int, request_body: ContactUpdate, db: Async
     # If the update was successful, return the updated contact
     updated_contact = await db.get(Contact, contact_id)
     return updated_contact
+
+async def remove_contact(contact_id: int, db: AsyncSession):
+    stmt = delete(Contact).where(Contact.id == contact_id)
+    
+    result = await db.execute(stmt)
+    await db.commit()
+    
+    if result.rowcount == 0:
+        return None
+    return {"message": "Contact deleted successfully"}
